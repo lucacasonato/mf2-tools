4320fb06a1c08461381eccbb51a8c5eff8cc6cf9
Luca Casonato <hello@lcas.dev>
2024-09-28 23:22:37 +0200

    Integrate vscode extension into repo

---
+++ tools/vscode/build.ts
@@ -0,0 +1,11 @@
+import { js, standalone } from "./grammar.ts";
+
+Deno.writeTextFileSync(
+  new URL(import.meta.resolve("../../vscode/syntaxes/mf2.tmLanguage.json")),
+  JSON.stringify(standalone, null, 2),
+);
+
+Deno.writeTextFileSync(
+  new URL(import.meta.resolve("../../vscode/syntaxes/mf2.js.json")),
+  JSON.stringify(js, null, 2),
+);
