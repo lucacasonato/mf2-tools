6f569bc6917f62f88f9448c15602eac9f79335b9
Nicolò Ribaudo <hello@nicr.dev>
2024-10-13 13:09:38 +0200

    [grammar] Allow multiple consecutive spaces (#89)

--- tools/vscode/build.ts
+++ tools/vscode/build.ts
@@ -2,10 +2,10 @@ import { js, standalone } from "./grammar.ts";
 
 Deno.writeTextFileSync(
   new URL(import.meta.resolve("../../vscode/syntaxes/mf2.tmLanguage.json")),
-  JSON.stringify(standalone, null, 2),
+  JSON.stringify(standalone, null, 2) + "\n",
 );
 
 Deno.writeTextFileSync(
   new URL(import.meta.resolve("../../vscode/syntaxes/mf2.js.json")),
-  JSON.stringify(js, null, 2),
+  JSON.stringify(js, null, 2) + "\n",
 );
